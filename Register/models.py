19fe861f04a9cd29ef93a8f1ac068e420346dcdc
Stanley <khoahd@d-soft.com.vn>
2020-06-02 10:26:46 +0700

    project2

--- Register/models.py
+++ Register/models.py
@@ -1,45 +1,32 @@
 import os
-
+from sqlalchemy.orm import relationship
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
-
-
 class User(db.Model):
-    __tablename__ = "Users"
+    __tablename__ = "users"
     # __table_args__ = (db.UniqueConstraint('name'),)
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False,unique=True)
     password = db.Column(db.String, nullable=False)
-    blogs = db.relationship("Blog", backref="Users", lazy=True)
-    comments = db.relationship("Comment", backref="Users", lazy=True)
-    def add_blog(self, title, date):
-        b = Blog(title=title, users=self.name,date=date)
-        db.session.add(b)
-        db.session.commit()
-
-class Blog(db.Model):
-    __tablename__ = "Blogs"
-    __table_args__ = (db.UniqueConstraint('title'),)
+    channels = relationship("Channel", secondary="tags")
+    messages = db.relationship("Message", backref="users", lazy=True)
+class Channel(db.Model):
+    __tablename__ = "channels"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False,unique=True)
-    content = db.Column(db.String, nullable=False)
-    ratings_count = db.Column(db.Integer, nullable=True)
-    date = db.Column(db.String,nullable=False)
-    Author = db.Column(db.String,db.ForeignKey("Users.name"),nullable=False)
-    comments = db.relationship("Comment", backref="Blogs", lazy=True)
-    def add_comment(self,content):
-        c = Comment(content)
-        db.session.add(c)
-        db.session.commit()
-
-
-class Comment(db.Model):
-    __tablename__ = "Comments"
+    users = relationship("User", secondary="tags")
+class Tag(db.Model):
+    __tablename__ = "tags"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    channel_id = db.Column(db.Integer, db.ForeignKey('channels.id'))
+    user = relationship(User, backref=backref("tags", cascade="all, delete-orphan"))
+    channel = relationship(Channel, backref=backref("tags", cascade="all, delete-orphan"))
+    comments = db.relationship("Message", backref="channels", lazy=True)
+class Message(db.Model):
+    __tablename__ = "messages"
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String, nullable=False)
-    date = db.Column(db.String, nullable=True)
-    user = db.Column(db.String,db.ForeignKey("Users.name"),nullable=False)
-    blog = db.Column(db.String,db.ForeignKey("Blogs.title"),nullable=False)
-    
+    user = db.Column(db.String,db.ForeignKey("users.name"),nullable=False)
+    channel = db.Column(db.String,db.ForeignKey("channels.title"),nullable=False)
