3488e7b307c9de4d580127c532f42d5fa17886e2
Stanley <khoahd@d-soft.com.vn>
2020-06-02 17:22:47 +0700

    khoa

--- Register/application.py
+++ Register/application.py
@@ -12,6 +12,8 @@ app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 app.secret_key = "abc"  
 db.init_app(app)
 list_drone = [] 
+title_name = []
+
 
 def main():
     db.create_all()
@@ -32,9 +34,13 @@ def register():
     # Get form information.
     name = request.form.get("name")
     password = request.form.get("password")
+    print("--------------NAME------------")
+    print(name)
 
+    result = User.query.filter_by(name=name).first()
+    print("-----------------RESULT------------")
     # Add user
-    if User.query.filter_by(name=name).first() is None:
+    if result is None:
         new_user = User(name=name,password=password)
         db.session.add(new_user)
         db.session.commit()
@@ -48,18 +54,22 @@ def login():
     # displayname  = request.args.get("displayname")
     name = request.form.get("name")
     password = request.form.get("password")
-    print("---davaoroi-----")
+    # print("---davaoroi-----")
+    
     session['username']= name
     # if 'username' in session:
     #     user = session['username']
     #     print(user)
     # print("adfadfkasndfoasdnfasdf")
-    # # Add user
+    # # Add use
+    
     if User.query.filter_by(name=name,password=password).first() is None:
+
         return render_template("register.html")
     else:
         full_filename = os.path.join(app.config['UPLOAD_FOLDER'],'first.jpg')
         return render_template("home.html",user_image=full_filename,name=name)
+
 @app.route("/register")
 def turn_back_toregister():
     return render_template("register.html")
@@ -68,6 +78,37 @@ def turn_back_toregister():
 def logout():
     session.pop('username', None)
     return render_template("login.html")
+@app.route("/channel",methods=["POST","GET"])
+def channel():
+    alert = False
+    name = session['username']    
+    if request.method == "POST":
+        channelname = request.form.get('channelname')
+        print('--------------channelname----------')
+        add_channel = Channel.query.filter_by(title=channelname).first()
+        if add_channel is None:
+            user = User.query.filter_by(name=name)[0]
+            new_channel = Channel(title=channelname)
+            user.channels.append(new_channel)
+            db.session.add(new_channel)
+            db.session.commit()
+        else:
+            alert = True
+    list_channel = Channel.query.all()
+    if len(list_channel)> 0 :
+        print('-------vo roi ne --------')
+        message = True
+    else : 
+        print('-------vo roi ne  hai --------')
+        message  = False
+    print("------------listchannel---------------")
+    print(list_channel)
+    print(message)
+    return render_template("channel.html",alert=alert,channel=list_channel,message=message)
+@app.route("/channel_detail")
+def channel_detail():
+    
+
 
 
 if __name__ == "__main__":
